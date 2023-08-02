47b293e92b5071ba4b200ed4230e3beff4ce6195
rocketRexTech <jordan.dotpetty@gmail.com>
2023-08-02 01:37:47 -0400

    more href updates

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -20,9 +20,9 @@ const Navigation = ({ account, setAccount }) => {
     return (
         <nav>
             <ul className='nav__links'>
-                <li><a href="https://github.com/rocketRexTech/Blockchain-Real-Estate-App" target='_blank'>Buy</a></li>
-                <li><a href="https://github.com/rocketRexTech/Blockchain-Real-Estate-App" target='_blank'>Rent</a></li>
-                <li><a href="https://github.com/rocketRexTech/Blockchain-Real-Estate-App" target='_blank'>Sell</a></li>
+                <li><a href="https://github.com/rocketRexTech/Blockchain-Real-Estate-App" target='_blank' rel="noopener">Buy</a></li>
+                <li><a href="https://github.com/rocketRexTech/Blockchain-Real-Estate-App" target='_blank' rel="noopener">Rent</a></li>
+                <li><a href="https://github.com/rocketRexTech/Blockchain-Real-Estate-App" target='_blank' rel="noopener">Sell</a></li>
             </ul>
 
             <div className='nav__brand'>
